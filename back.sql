92e1be86d19c12ba23a6eab21c55ca4c04e92cc2
Vivek Vinushanth <32198547+VivekVinushanth@users.noreply.github.com>
2018-11-07 20:34:31 +0530

    Update back.sql

--- back.sql
+++ back.sql
@@ -2,31 +2,31 @@ drop database if exists angaadi;
 CREATE database angaadi ;
 use angaadi;
 
-CREATE TABLE `Customer` (
-  `customer_ID` int NOT NULL AUTO_INCREMENT,
-  `Email_ID` varchar(255),
-  `UserName` varchar(255) ,
-  `Street_name` varchar(255),
-  `City` varchar(255),
-  `FirstName` varchar(255),
-  `LastName` varchar(255),
-  PRIMARY KEY (`customer_ID`) 
+CREATE TABLE Customer (
+  customer_id int NOT NULL AUTO_INCREMENT,
+  email_id varchar(255),
+  username varchar(255) ,
+  street_name varchar(255),
+  city varchar(255),
+  firstname varchar(255),
+  lastname varchar(255),
+  PRIMARY KEY (customer_id) 
 );
 
-CREATE TABLE `Orders` (
-  `order_ID` int,
-  `Total_Price` numeric(9,2),
-  `Order_date` datetime,
-  `Delivery_Method` enum("Home Delivery","Store Pickup"),
-  `customer_ID` int,
-  PRIMARY KEY (`order_ID`),
-  Foreign KEY (`customer_ID`) references `Customer`(`customer_ID`)
+CREATE TABLE orders (
+  order_id int,
+  total_price numeric(9,2),
+  order_date datetime,
+  delivery_method enum("Home Delivery","Store Pickup"),
+  customer_id int,
+  PRIMARY KEY (order_id),
+  Foreign KEY (customer_id) references customer(customer_id)
 );
 
-CREATE TABLE `Shipping_Address` (
-  `order_ID` int,
-  `City` varchar(255),
-  `Zip_Code` varchar(255),
+CREATE TABLE shipping_address (
+  order_id int,
+  city varchar(255),
+  zip_code varchar(255),
   `Address_Line1` varchar(255),
   `Address_Line2` varchar(255),
   Primary KEY (`order_ID`),
@@ -54,13 +54,12 @@ CREATE TABLE `Product` (
 CREATE TABLE `Product_Variant` (
   `SKU` int,
   `product_ID` int,
-  `order_ID` int,
   `stock` int,
   `unit_Price` numeric(9,2),
   `weight` numeric(5,2),
    `Image` blob,
   PRIMARY KEY (`SKU`),
-   Foreign KEY (`product_ID`) references `Product`(`product_ID`)
+  Foreign KEY (`product_ID`) references `Product`(`product_ID`)
 );
 
 
@@ -68,7 +67,7 @@ CREATE TABLE `Variant_Detail` (
   `SKU` int,
   `Attribute_Name` varchar(255),
   `Attribute_Value` varchar(255),
-  Primary KEY(`SKU`),
+  Primary KEY(`SKU`,`Attribute_Name`),
   Foreign KEY (`SKU`) references `Product_Variant`(`SKU`)
 );
 
@@ -80,12 +79,9 @@ CREATE TABLE `Category` (
   PRIMARY KEY (`pid`)
 );
 
-
-
-
 CREATE TABLE `Category_Products` (
-  `pid` varchar(255),
-  `product_ID`int,
+  `pid` int,
+  `product_ID` int,
   PRIMARY KEY  (`pid`, `product_ID`),
   Foreign KEY (`pid`) references `Category` (`pid`) ,
   Foreign KEY (`product_ID`) references `Product`(`product_ID`)
