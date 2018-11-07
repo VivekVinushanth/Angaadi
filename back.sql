1fc3162941ccef83541bc31d5824632b6026762e
Narththanan <38082492+Narththanan@users.noreply.github.com>
2018-11-07 17:51:40 +0530

    Update back.sql

--- back.sql
+++ back.sql
@@ -1,5 +1,5 @@
-
-CREATE database if not exists angaadi ;
+drop database if exists angaadi;
+CREATE database angaadi ;
 use angaadi;
 
 CREATE TABLE `Customer` (
@@ -49,6 +49,27 @@ CREATE TABLE `Product` (
   `brand` varchar(255),
   PRIMARY KEY (`product_ID`)
 );
+insert into Product values(10000,'xiaomi Redmi Note 5','Xiaomi');
+insert into Product values(10001,'xiaomi Redmi S2','Xiaomi');
+insert into Product values(10002,'ASUS FX63VD Notebook','ASUS');
+insert into Product values(10003,'ASUS ROG GFX72VM6700','ASUS');
+insert into Product values(10004,'Teclast F6 Pro','TECLAST');
+insert into Product values(10005,'Xiaomi Mi Notebook Pro','Xiaomi');
+insert into Product values(10006,'Y1 M1 Mirrorless Digital Camera','Y1');
+insert into Product values(10007,'SONY DSC-W800 20 MP Digital Camera','SONY');
+insert into Product values(10008,'PROTAX D7100 33MP Professional DSLR Shape Digital Camera','PROTAX');
+insert into Product values(10009,'New Canon EOS 1300D Rebel T6 DSLR Wi-Fi Camera','Canon');
+insert into Product values(10010,'HARO BMX Intermediate Street Performance Bike','HARO');
+insert into Product values(10011,'X-Front Downhill off-road Bicycle','X-Front');
+insert into Product values(10012,'Running Leopard mountain bike','Running Leopard');
+insert into Product values(10013,'HONHX Sports Watch','HONHX');
+insert into Product values(10014,'SPRAOI 2018 Luxury Sport Watch','SPRAOI');
+insert into Product values(10015,'shshd Business Skeleton Watch','shshd');
+insert into Product values(10016,'contena 2018 Women Watches Romantic Stainless Steel','contena');
+insert into Product values(10017,'PAUL VALENTINE Luxury Quartz Wrist watch','PAUL VALENTINE');
+insert into Product values(10018,'Daniel Sabrina 2018 Women Quartz  wrist Watch men Style','Daniel Sabrina');
+insert into Product values(10019,'OTOKY 2018 Dress Watches Faux Chronograph Quartz Casual','OTOKY');
+
 
 CREATE TABLE `Product_Variant` (
   `SKU` int,
@@ -61,6 +82,9 @@ CREATE TABLE `Product_Variant` (
   PRIMARY KEY (`SKU`),
    Foreign KEY (`product_ID`) references `Product`(`product_ID`)
 );
+
+
+
 
 CREATE TABLE `Variant_Detail` (
   `SKU` int,
@@ -78,6 +102,27 @@ CREATE TABLE `Category` (
   PRIMARY KEY (`category_name`,`sub_category_name`),
   Foreign KEY  (`product_ID`) references `Product`(`product_ID`)
 );
+
+insert into Category values('Cellphones & Telecommunications','Mobile Phones',10000);
+insert into Category values('Cellphones & Telecommunications','Mobile Phones',10001);
+insert into Category values('Computer & Office','Gaming Laptops',10002);
+insert into Category values('Computer & Office','Gaming Laptops',10003);
+insert into Category values('Computer & Office','Laptops',10004);
+insert into Category values('Computer & Office','Laptops',10005);
+insert into Category values('Consumer Electronics','Digital Cameras',10006);
+insert into Category values('Consumer Electronics','Digital Cameras',10007);
+insert into Category values('Consumer Electronics','Digital Cameras',10008);
+insert into Category values('Consumer Electronics','Digital Cameras',10009);
+insert into Category values('Sports & Entertainment','Bicycle',10010);
+insert into Category values('Sports & Entertainment','Bicycle',10011);
+insert into Category values('Sports & Entertainment','Bicycle',10012);
+insert into Category values('Watches','Men Watches',10013);
+insert into Category values('Watches','Men Watches',10014);
+insert into Category values('Watches','Men Watches',10015);
+insert into Category values('Watches','Men Watches',10016);
+insert into Category values('Watches','Men Watches',10017);
+insert into Category values('Watches','Men Watches',10018);
+insert into Category values('Watches','Men Watches',10019);
 
 CREATE TABLE `Category_Products` (
   `category_name` varchar(255),
@@ -140,4 +185,3 @@ CREATE TABLE `Customer_Telephone` (
   PRIMARY KEY `customer_ID`,
    FOREIGN KEY (`customer_ID`) references `Customer`(customer_ID`)
 );
-
