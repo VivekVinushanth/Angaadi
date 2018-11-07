79ba326cae4738cbcb70d07059e4552aedd0ebae
Narththanan <38082492+Narththanan@users.noreply.github.com>
2018-11-07 19:18:49 +0530

    Update back.sql

--- back.sql
+++ back.sql
@@ -49,6 +49,26 @@ CREATE TABLE `Product` (
   `brand` varchar(255),
   PRIMARY KEY (`product_ID`)
 );
+insert into Product values(10000,'xiaomi Redmi Note 5','Xiaomi');
+insert into Product values(10001,'xiaomi Redmi S2','Xiaomi');
+insert into Product values(10002,'ASUS FX63VD Notebook','ASUS');
+insert into Product values(10003,'ASUS ROG GFX72VM6700','ASUS');
+insert into Product values(10004,'Teclast F6 Pro','TECLAST');
+insert into Product values(10005,'Xiaomi Mi Notebook Pro','Xiaomi');
+insert into Product values(10006,'Y1 M1 Mirrorless Digital Camera','Y1');
+insert into Product values(10007,'SONY DSC-W800 20 MP Digital Camera','SONY');
+insert into Product values(10008,'PROTAX D7100 33MP Professional DSLR Shape Digital Camera','PROTAX');
+insert into Product values(10009,'New Canon EOS 1300D Rebel T6 DSLR Wi-Fi Camera','Canon');
+insert into Product values(10010,'HARO BMX Intermediate Street Performance Bike','HARO');
+insert into Product values(10011,'X-Front Downhill off-road Bicycle','X-Front');
+insert into Product values(10012,'Running Leopard mountain bike','Running Leopard');
+insert into Product values(10013,'HONHX Sports Watch','HONHX');
+insert into Product values(10014,'SPRAOI 2018 Luxury Sport Watch','SPRAOI');
+insert into Product values(10015,'shshd Business Skeleton Watch','shshd');
+insert into Product values(10016,'contena 2018 Women Watches Romantic Stainless Steel','contena');
+insert into Product values(10017,'PAUL VALENTINE Luxury Quartz Wrist watch','PAUL VALENTINE');
+insert into Product values(10018,'Daniel Sabrina 2018 Women Quartz  wrist Watch men Style','Daniel Sabrina');
+insert into Product values(10019,'OTOKY 2018 Dress Watches Faux Chronograph Quartz Casual','OTOKY');
 
 
 CREATE TABLE `Product_Variant` (
@@ -64,6 +84,7 @@ CREATE TABLE `Product_Variant` (
 );
 
 
+
 CREATE TABLE `Variant_Detail` (
   `SKU` int,
   `Attribute_Name` varchar(255),
@@ -72,23 +93,51 @@ CREATE TABLE `Variant_Detail` (
   Foreign KEY (`SKU`) references `Product_Variant`(`SKU`)
 );
 
+
 CREATE TABLE `Category` (
   `category_name` varchar(255),
   `sub_category_name` varchar(255),
-  `product_ID` int,
-  PRIMARY KEY (`category_name`,`sub_category_name`),
-  Foreign KEY  (`product_ID`) references `Product`(`product_ID`)
+  PRIMARY KEY (`category_name`,`sub_category_name`)
 );
 
+insert into Category values('Cellphones & Telecommunications','Mobile Phones');
+insert into Category values('Computer & Office','Gaming Laptops');
+insert into Category values('Computer & Office','Laptops');
+insert into Category values('Consumer Electronics','Digital Cameras');
+insert into Category values('Sports & Entertainment','Bicycle');
+insert into Category values('Watches','Men Watches');
+insert into Category values('Watches','Women Watches');
+
+
 CREATE TABLE `Category_Products` (
-  `category_name` varchar(255),
   `sub_category_name` varchar(255),
   `product_ID`int,
-  PRIMARY KEY  (`category_name`, `sub_category_name`, `product_ID`),
-  Foreign KEY (`category_name`) references `Category` (`category_name`) ,
+  PRIMARY KEY  (`sub_category_name`, `product_ID`),
   Foreign KEY (`sub_category_name`) references `Category` (`sub_category_name`) ,
   Foreign KEY (`product_ID`) references `Product`(`product_ID`)
 );
+
+insert into Category_Products values('Mobile Phones',10000);
+insert into Category_Products values('Mobile Phones',10001);
+insert into Category_Products values('Gaming Laptops',10002);
+insert into Category_Products values('Gaming Laptops',10003);
+insert into Category_Products values('Laptops',10004);
+insert into Category_Products values('Laptops',10005);
+insert into Category_Products values('Digital Cameras',10006);
+insert into Category_Products values('Digital Cameras',10007);
+insert into Category_Products values('Digital Cameras',10008);
+insert into Category_Products values('Digital Cameras',10009);
+insert into Category_Products values('Bicycle',10010);
+insert into Category_Products values('Bicycle',10011);
+insert into Category_Products values('Bicycle',10012);
+insert into Category_Products values('Men Watches',10013);
+insert into Category_Products values('Men Watches',10014);
+insert into Category_Products values('Men Watches',10015);
+insert into Category_Products values('Women Watches',10016);
+insert into Category_Products values('Women Watches',10017);
+insert into Category_Products values('Women Watches',10018);
+insert into Category_Products values('Women Watches',10019);
+
 
 CREATE TABLE `Cart` (
   `Date_Added` datetime,
@@ -108,6 +157,9 @@ CREATE TABLE `Freight_Details` (
   PRIMARY KEY (`Tracking_ID`),
   Foreign KEY (`order_ID`) references `Orders`(`order_ID`)
 );
+
+
+
 
 CREATE TABLE `Order_Detail` (
   `SKU` int,
