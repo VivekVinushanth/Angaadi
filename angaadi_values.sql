f8b2d75dc4de68b3ecae61e75ca883c1e8826e65
Vivek Vinushanth <32198547+VivekVinushanth@users.noreply.github.com>
2018-11-07 18:49:37 +0530

    Add files via upload

---
+++ angaadi_values.sql
@@ -0,0 +1,45 @@
+insert into Category values('Cellphones & Telecommunications','Mobile Phones',10000);
+insert into Category values('Cellphones & Telecommunications','Mobile Phones',10001);
+insert into Category values('Computer & Office','Gaming Laptops',10002);
+insert into Category values('Computer & Office','Gaming Laptops',10003);
+insert into Category values('Computer & Office','Laptops',10004);
+insert into Category values('Computer & Office','Laptops',10005);
+insert into Category values('Consumer Electronics','Digital Cameras',10006);
+insert into Category values('Consumer Electronics','Digital Cameras',10007);
+insert into Category values('Consumer Electronics','Digital Cameras',10008);
+insert into Category values('Consumer Electronics','Digital Cameras',10009);
+insert into Category values('Sports & Entertainment','Bicycle',10010);
+insert into Category values('Sports & Entertainment','Bicycle',10011);
+insert into Category values('Sports & Entertainment','Bicycle',10012);
+insert into Category values('Watches','Men Watches',10013);
+insert into Category values('Watches','Men Watches',10014);
+insert into Category values('Watches','Men Watches',10015);
+insert into Category values('Watches','Men Watches',10016);
+insert into Category values('Watches','Men Watches',10017);
+insert into Category values('Watches','Men Watches',10018);
+insert into Category values('Watches','Men Watches',10019);
+
+insert into Product values(10000,'xiaomi Redmi Note 5','Xiaomi');
+insert into Product values(10001,'xiaomi Redmi S2','Xiaomi');
+insert into Product values(10002,'ASUS FX63VD Notebook','ASUS');
+insert into Product values(10003,'ASUS ROG GFX72VM6700','ASUS');
+insert into Product values(10004,'Teclast F6 Pro','TECLAST');
+insert into Product values(10005,'Xiaomi Mi Notebook Pro','Xiaomi');
+insert into Product values(10006,'Y1 M1 Mirrorless Digital Camera','Y1');
+insert into Product values(10007,'SONY DSC-W800 20 MP Digital Camera','SONY');
+insert into Product values(10008,'PROTAX D7100 33MP Professional DSLR Shape Digital Camera','PROTAX');
+insert into Product values(10009,'New Canon EOS 1300D Rebel T6 DSLR Wi-Fi Camera','Canon');
+insert into Product values(10010,'HARO BMX Intermediate Street Performance Bike','HARO');
+insert into Product values(10011,'X-Front Downhill off-road Bicycle','X-Front');
+insert into Product values(10012,'Running Leopard mountain bike','Running Leopard');
+insert into Product values(10013,'HONHX Sports Watch','HONHX');
+insert into Product values(10014,'SPRAOI 2018 Luxury Sport Watch','SPRAOI');
+insert into Product values(10015,'shshd Business Skeleton Watch','shshd');
+insert into Product values(10016,'contena 2018 Women Watches Romantic Stainless Steel','contena');
+insert into Product values(10017,'PAUL VALENTINE Luxury Quartz Wrist watch','PAUL VALENTINE');
+insert into Product values(10018,'Daniel Sabrina 2018 Women Quartz  wrist Watch men Style','Daniel Sabrina');
+insert into Product values(10019,'OTOKY 2018 Dress Watches Faux Chronograph Quartz Casual','OTOKY');
+
+
+
+
