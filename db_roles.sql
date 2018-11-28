33c4adde96bef80c39f8dfdf780cb8a2fc55351b
jeyakeethan <jeyakeethan@gmail.com>
2018-11-28 20:49:59 +0530

    Update db_roles.sql

--- db_roles.sql
+++ db_roles.sql
@@ -1,11 +1,13 @@
+
+
+
 CREATE USER IF NOT EXISTS 'public_access' IDENTIFIED BY '0000';
 CREATE USER IF NOT EXISTS 'staff' IDENTIFIED BY '1111';
 CREATE USER IF NOT EXISTS 'courier_side' IDENTIFIED BY '2222';
 CREATE USER IF NOT EXISTS 'admin' IDENTIFIED BY '3333';
+GRANT SELECT,INSERT,DELETE,UPDATE ON *.* TO public_access;
 
-
-GRANT SELECT,INSERT,DELETE,UPDATE ON angaadi.cart TO public_access;
-GRANT SELECT,INSERT,DELETE,UPDATE ON angaadi_users.users TO public_access;
+GRANT SELECT,INSERT,DELETE,UPDATE ON angaadi_users.* TO public_access;
 GRANT SELECT,INSERT,DELETE,UPDATE ON angaadi.Product_Variant TO public_access;
 GRANT SELECT ON angaadi.main_city TO public_access;
 GRANT SELECT,INSERT,DELETE,UPDATE ON angaadi.variant_detail TO public_access;
@@ -17,6 +19,7 @@ GRANT SELECT,INSERT,DELETE,UPDATE ON angaadi.guest TO public_access;
 GRANT SELECT,INSERT,DELETE,UPDATE ON angaadi.orders TO public_access;
 GRANT SELECT,INSERT,DELETE,UPDATE ON angaadi.payment TO public_access;
 GRANT SELECT,INSERT,DELETE,UPDATE ON angaadi.shipping_address TO public_access;
+GRANT SELECT,INSERT,DELETE,UPDATE ON angaadi.cart TO public_access;
 
 GRANT SELECT,INSERT,DELETE,UPDATE ON *.* TO staff;
 GRANT SELECT,INSERT,DELETE,UPDATE ON *.* TO admin;
