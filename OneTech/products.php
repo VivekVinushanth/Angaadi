3501908598589095c0b13308a66fbe2e5a0b227e
jeyakeethan <jeyakeethan@gmail.com>
2018-11-28 11:09:18 +0530

    Update products.php

--- OneTech/products.php
+++ OneTech/products.php
@@ -24,8 +24,8 @@
 		$query = "SELECT * FROM Product_Variant NATURAL JOIN Product NATURAL JOIN category_products NATURAL JOIN category WHERE category_name='$category'";
 	}
 	else if(isset($_GET['subcategory'])){
-		$sub_catergory = $_GET['subcategory'];
-		$query = "SELECT * FROM Product_Variant NATURAL JOIN Product NATURAL JOIN category_products WHERE sub_category_name='$sub_category'";
+		$sub_category = $_GET['subcategory'];
+		$query = "SELECT * FROM Product_Variant NATURAL JOIN Product NATURAL JOIN category_products NATURAL JOIN category WHERE sub_category_name='$sub_category'";
 	}
 	else{
 		$query = "SELECT * FROM Product_Variant NATURAL JOIN Product";
